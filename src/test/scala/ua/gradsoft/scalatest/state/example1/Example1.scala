ade6ce0c7673dd4523f81f73f6a9ca8854b1f450
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2012-02-15 15:43:06 +0200

    added 'zero' test.

---
+++ src/test/scala/ua/gradsoft/scalatest/state/example1/Example1.scala
@@ -0,0 +1,68 @@
+package ua.gradsoft.scalates.state.example1
+
+import org.scalatest._
+import ua.gradsoft.scalatest.state._
+
+
+/**
+ * This is formal minimal example, where state
+ * represented by int.
+ **/
+object Example1StatesInfo extends FixtureStateInfo
+{
+
+  type FixtureType = Int;
+
+  object States extends Enumeration
+  {
+    val ONE, TWO, THREE = Value;
+  }
+
+  val startStates = States;
+
+  override def load(s: StartStateType): FixtureType = 
+  {
+   import States._
+   s match {
+      case ONE => 1
+      case TWO => 2
+      case THREE => 3
+   }
+  }
+
+}
+
+
+//class Example1TestSuites extends StatefullFixture.Suite(Examples1StatesInfo)
+class Example1TestSuites extends Suite
+{
+
+  /**
+   *test that load state and do nothing.
+   **/
+/*
+  state test("check-one") in(ONE) change nothing in {
+    val f = fixture();
+    f should be 1
+  }
+*/
+
+//  state test("check-any") in(ANY) change nothing in {
+
+  /**
+   * test that change state t
+   **/
+/*
+  state test("one -> two") in(ONE) out(TWO) change nothing in {
+    val f = fixture();
+    f should be 1
+    
+  }
+*/
+
+
+}
+
+
+
+// vim: set ts=4 sw=4 et:
